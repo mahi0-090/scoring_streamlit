7c102b0e8e55b3b30cd14041ab9e960ca67cc661
mahi0-090 <101629958+mahi0-090@users.noreply.github.com>
2022-03-18 21:02:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
-echo "\
+echo "[theme]
+primaryColor=\"#f21111\"
+backgroundColor=\"#0e1117\"
+secondaryBackgroundColor=\"#31333F\"
+textColor=\"#fafafa\"
+font=\"sans serif\"
+\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
